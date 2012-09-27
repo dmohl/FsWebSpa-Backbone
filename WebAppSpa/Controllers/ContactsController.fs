89dc47a9e6dfefca2ddb733a305a647d3ea122cf
= <dmohl@yahoo.com>
2012-09-27 09:14:20 -0500

    Created

---
+++ WebAppSpa/Controllers/ContactsController.fs
@@ -0,0 +1,28 @@
+﻿namespace FsWeb.Controllers
+
+open System.Collections.Generic
+open System.Web
+open System.Web.Mvc
+open System.Net.Http
+open System.Web.Http
+open FsWeb.Models
+
+type ContactsController() =
+    inherit ApiController()
+
+    // This is for demonstration purposes only. 
+    let contacts = 
+        List<Contact>
+            ( [|
+                Contact(FirstName = "John", LastName = "Doe", Phone = "123-123-1233")
+                Contact(FirstName = "Jane", LastName = "Doe", Phone = "123-111-9876")
+              |] ) 
+
+    // GET /api/contacts
+    member x.Get() = 
+        // TODO: Replace with your code to retrieve the contacts list
+        contacts
+    // POST /api/contacts
+    member x.Post ([<FromBody>] contact:Contact) = 
+        // TODO: Replace with your code to persiste the contact information
+        contacts.Add contact
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a simple example of a Single Page Application built with C#, F#, ASP.NET Web API, and Backbone.
